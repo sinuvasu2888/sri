d0b754a79a20f82ebe17e9228b492c474e5fbd10
sri <ksbjfk@gmail.com>
2017-06-07 17:57:27 -0500

    Array of names

--- HelloGit/HelloGit.java
+++ HelloGit/HelloGit.java
@@ -4,7 +4,22 @@ public class HelloGit {
 	public static void main(String[] args)
 	{
 		//a greeting
-		System.out.println("Hello,git!");
+		//System.out.println("Hello,git!");
+		
+		
+		String[] names = { "Bibo", "Frodo" , "Aragorn" , "Legolas " , "Gandalf" };
+		
+		//Print Greeting 
+		
+		for(int i=0;i<=names.length;i++)
+		{
+			
+			System.out.format("Hello" , names[i]);
+			
+		}
+		
+		
+		
 	}
 
 }
